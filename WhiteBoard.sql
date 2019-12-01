f4f9207fac448a387ca630588c8152b509dd0497
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-11-30 17:09:27 -0800

    update db schema

--- WhiteBoard.sql
+++ WhiteBoard.sql
@@ -2,18 +2,29 @@ CREATE DATABASE WhiteBoard;
 
 USE WhiteBoard;
 
+CREATE TABLE Visitors(
+	visitorID INT(11) NOT NULL Primary key AUTO_INCREMENT, 
+	userName VARCHAR(255) NOT NULL,
+	numQueuesIn INT not null,
+	queuesWaitingIn Varchar(255) not null	
+);
+
 CREATE TABLE Users(
 	UserID INT(11) NOT NULL Primary key AUTO_INCREMENT, 
 	userName VARCHAR(255) NOT NULL,
-	hashedPass VARCHAR(255) NOT NULL,
-	numQueuesIn INT(11) not null,
-	queuesWaitingIn Varchar(255) not null,	
-	isAdminOf VARCHAR(1000) not null
+	hashedPass VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Queues(
 	queueID INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	queueName VARCHAR(255) NOT NULL,
-	queueOrder VARCHAR(2500) Not null ,
-	adminList VARCHAR(1000) not null
+	queueOrder VARCHAR(2500) Not null
+);
+
+CREATE TABLE Admins(
+	AdminID INT(11) NOT NULL Primary key AUTO_INCREMENT, 
+	UserID INT(11) NOT NULL, 
+	queueID INT(11) NOT NULL,
+	FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (queueID) REFERENCES Queues(queueID)
 );
