ddb5815e2c9b502335e81450464cff2569afefc0
Audrey (Adrielo) Bongalon <bongalon@usc.edu>
2019-12-03 23:02:23 -0800

    updated initSQL for hashed passwords

--- InitData.sql
+++ InitData.sql
@@ -2,31 +2,32 @@ Use WhiteBoard;
 
 INSERT INTO Visitors (userName, numQueuesIn, queuesWaitingIn)
 VALUES
-    ("ttrojan", 1, "CSCI201"),
-    ("jbruin", 2, "CSCI201,CSCI270"),
-    ("ttraveler", 1, "CSCI201");
+("ttrojan", 1, "CSCI201"),
+("jbruin", 2, "CSCI201,CSCI270"),
+("ttraveler", 1, "CSCI201");
 
 INSERT INTO Queues (queueName, queueOrder)
 VALUES
-    ("CSCI201", "ttrojan,ttraveler,jbruin"),
-    ("CSCI270", "jbruin");
+("CSCI201", "ttrojan,ttraveler,jbruin"),
+("CSCI270", "jbruin");
 
 INSERT INTO Users (userName, hashedPass)
 VALUES
-    ("testcp", "password"),
-    ("testcp1", "password");
+("testcp", "lJE8LUgEgNwffkqNInjxfvgkmqw6l7PzJ3C6nV0E9yo="),		-- testcp,  password
+("testcp1", "LSEqy58yvP+KssY9jraz+b7b0YUfv4GZfsV9UnAFOJg="),	-- testcp1, password
+("a", "lhtt0+3jy47LqsvWjeBAzXjrLtWIkTDM60xJJo6k1QY=");			-- a,       a
 
 INSERT INTO Admins (UserID, queueID)
-VALUES 
+VALUES
 (
-	(SELECT UserID FROM Users WHERE userName='testcp'),
-	(SELECT queueID FROM Queues WHERE queueName='CSCI270')
+    (SELECT UserID FROM Users WHERE userName='testcp'),
+    (SELECT queueID FROM Queues WHERE queueName='CSCI270')
 ),
 (
-	(SELECT UserID FROM Users WHERE userName='testcp'),
-	(SELECT queueID FROM Queues WHERE queueName='CSCI201')
+    (SELECT UserID FROM Users WHERE userName='testcp'),
+    (SELECT queueID FROM Queues WHERE queueName='CSCI201')
 ),
 (
-	(SELECT UserID FROM Users WHERE userName='testcp1'),
-	(SELECT queueID FROM Queues WHERE queueName='CSCI270')
+    (SELECT UserID FROM Users WHERE userName='testcp1'),
+    (SELECT queueID FROM Queues WHERE queueName='CSCI270')
 );
